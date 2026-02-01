e5b6a03339413794506da969323d49c31505f7e9
aloshhen <166549924+aloshhen@users.noreply.github.com>
2026-02-02 00:09:09 +0100

    Add tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+export default {
+  content: ['./index.html', './src/**/*.{js,jsx}'],
+  theme: {
+    extend: {
+      colors: {
+        'hot-pink': '#FF69B4',
+        'neon-pink': '#FF10F0',
+        'fuchsia': '#FF00FF',
+      },
+      fontFamily: {
+        'pixel': ['"Press Start 2P"', 'cursive'],
+      },
+    },
+  },
+  plugins: [],
+}
